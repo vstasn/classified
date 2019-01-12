65439f19758de098d12f136a6cca77749e436e37
vstasn <vstasn@gmail.com>
2019-01-12 12:29:24 +0500

    Remove static and move to CDN

--- classified/settings.py
+++ classified/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 # Counter settings
 COUNTER_ADS_VIEW = 'AdsDetailView'
